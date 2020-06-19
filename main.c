2e299277805f314a9a08fa7b07619d3dc8f9207b
Sebastian Riedel <git@basxto.de>
2020-06-19 22:07:11 +0200

    Add working wobbling effect

--- main.c
+++ main.c
@@ -6,43 +6,34 @@
 #include <gb/hardware.h>
 #include <stdio.h>
 
-const UINT8 empty = 0x00;
-const UINT8 full = 0x00;
-const UINT8 left = 0x80;
-const UINT8 right = 0x01;
-
-INT8 scanline_offsets[8];
-UINT8 scanline_counter;
+UINT8 scanline_offsets[8];
+UINT8 scanline_ly_offset;
 
 void scanline_isr() {
-    //SCX_REG += scanline_offsets[scanline_counter];
-    SCX_REG = (INT8)SCX_REG + scanline_offsets[scanline_counter];
-    scanline_counter = (UINT8)(scanline_counter+1)%8;
-    ++LYC_REG;
+    SCX_REG = scanline_offsets[(UINT8)((UINT8)LY_REG + scanline_ly_offset)%8];
+    LYC_REG = LY_REG+1;
 }
 
 void screen_wobble() {
-    scanline_offsets[0] = 1;
-    scanline_offsets[1] = 1;
-    scanline_offsets[2] = 1;
-    scanline_offsets[3] = 1;
-    scanline_offsets[4] = -1;
-    scanline_offsets[5] = -1;
-    scanline_offsets[6] = -1;
-    scanline_offsets[7] = -1;
+    scanline_offsets[0] = 4;
+    scanline_offsets[1] = 5;
+    scanline_offsets[2] = 6;
+    scanline_offsets[3] = 7;
+    scanline_offsets[4] = 7;
+    scanline_offsets[5] = 6;
+    scanline_offsets[6] = 5;
+    scanline_offsets[7] = 4;
     STAT_REG = 0x45;
     LYC_REG = 0x00;
-    scanline_counter = 0;
+    scanline_ly_offset = 0;
 
     set_interrupts(VBL_IFLAG | LCD_IFLAG);
-    for(UINT8 i = 0; i < 20; ++i){
+    for(UINT8 i = 0; i < 40; ++i){
         wait_vbl_done();
         wait_vbl_done();
         wait_vbl_done();
         wait_vbl_done();
-        wait_vbl_done();
-        wait_vbl_done();
-        ++scanline_counter;
+        ++scanline_ly_offset;
     }
     set_interrupts(VBL_IFLAG);
     SCX_REG = 4;
